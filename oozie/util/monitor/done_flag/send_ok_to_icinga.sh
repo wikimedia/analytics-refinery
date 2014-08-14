4430d6ae1cd38b856f5356b444924485a67d3aed
Andrew Otto <acotto@gmail.com>
2014-08-14 14:43:40 -0400

    Add option to set icinga reported hostname to send_ok_to_icinga.sh script
    Icinga does not expect FDQNs of hosts.  This changes the default
    reported host of this script to just 'analytics1027', and also adds
    the ability to set the reported host via the CLI.
    
    Change-Id: I7b27901062f62d23c9c7434e6cbcdb2efd9216b6

--- oozie/util/monitor/done_flag/send_ok_to_icinga.sh
+++ oozie/util/monitor/done_flag/send_ok_to_icinga.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-usage="Usage: $(basename "$0") [--dry-run|-n] <icinga_service_description> <dataset_location>"
+usage="Usage: $(basename "$0") [--dry-run|-n] [--hostname|-h <icinga_reported_hostname>] <icinga_service_description> <dataset_location>"
 
 icinga_nsca_host='icinga.wikimedia.org'
 icinga_nsca_port='5667'
@@ -10,13 +10,26 @@ icinga_nsca_port='5667'
 # This is hardcoded, as any DataNode may run this script
 # and we want the service to always be associated with
 # the same node.
-icinga_reported_hostname='analytics1027.eqiad.wmnet'
+icinga_reported_hostname='analytics1027'
 
 dry_run='false'
-if [ "$1" == '-n' -o "$1" == '--dry-run' ]; then
-    dry_run='true'
+
+# Parse CLI args
+while [ $# -gt 2 ]; do
+    case "$1" in
+    -n|--dry-run)
+        dry_run='true'
+        ;;
+    -h|--hostname)
+        shift
+        icinga_reported_hostname="$1"
+        ;;
+    *)
+        break
+        ;;
+    esac
     shift
-fi
+done
 
 icinga_service_description="$1"
 location="$2"
