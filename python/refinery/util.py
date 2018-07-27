ab39a2a512b2d97f3096b5c8a24a7ae80dfbbdb6
Francisco Dans <fdans@wikimedia.org>
2018-07-27 09:53:55 -0500

    Add druid snapshot deletion script
    Adds a script to delete out of date snapshots stored in druid. Like
    our other deletion scripts this is supposed to be run periodically as
    a cron configured in puppet.
    
    Bug: T197889
    Change-Id: Ia4aa0cafd5b3a73969d48204ba23172ef6c2243b

--- python/refinery/util.py
+++ python/refinery/util.py
@@ -25,6 +25,9 @@ import os
 import subprocess
 import re
 import tempfile
+import urllib2
+import json
+
 # Ugly but need python3 support
 try:
     from urlparse import urlparse
@@ -718,3 +721,83 @@ class HdfsUtils(object):
         Returns the size in bytes of a hdfs path
         """
         return int(sh(['hdfs', 'dfs', '-du', '-s', path]).split()[0])
+
+class DruidUtils(object):
+    def __init__(self, druid_host):
+        self.host = druid_host
+        self.coordinator_url = self.host + '/druid/coordinator/v1'
+
+    def list_intervals(self, datasource):
+        """
+        Gets a list of the intervals that a druid datasource is composed of,
+        sorted descending chronologically. The intervals are in the following
+        format:
+        yyyy-MM-ddThh:mm:ss.fffZ/yyyy-MM-ddThh:mm:ss.fffZ
+        """
+        url = (self.coordinator_url + '/datasources/'
+               + datasource + '/intervals')
+        intervals = json.loads(self.get(url))
+        return intervals
+
+    def remove_interval(self, datasource, interval, dry_run=False):
+        """
+        Sends a DELETE request to the druid coordinator for the selected
+        datasource and interval.
+        """
+        url = (self.coordinator_url + '/datasources/'
+               + datasource + '?kill=true&interval=' + interval)
+        self.delete(url, dry_run=dry_run)
+
+    def remove_datasource(self, datasource, deep=False, dry_run=False):
+        """
+        If deep is false, just sends a DELETE request to the druid coordinator
+        for the selected datasource, therefore just disabling it but not deleting
+        it from deep storage. Returns the response body of the request if 
+        it was successful.
+
+        If deep is true, removes each interval in the datasource, effectively deleting
+        it from deep storage in hdfs.
+        """
+        url = (self.coordinator_url + '/datasources/'
+            + datasource)
+        self.delete(url, dry_run=dry_run)
+        # Removing the datasource from deep storage implies sending a kill signal
+        # to each of the segments of the datasource.
+        if deep:
+            for interval in self.list_intervals(datasource):
+                logger.info('Deleting interval {}...'.format(interval))
+                self.remove_interval(datasource, interval, dry_run)
+
+    def list_datasources(self):
+        url = self.coordinator_url + '/datasources'
+        datasources = json.loads(self.get(url))
+        return datasources
+
+    def delete(self, url, dry_run=False):
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request(url)
+        request.get_method = lambda: 'DELETE'
+        if dry_run:
+            print('DELETE ' + url)
+            return '{}'
+        else:
+            try:
+                return opener.open(request).read()
+            except urllib2.HTTPError as e:
+                logger.error('HTTPError = ' + str(e.code))
+            except urllib2.URLError as e:
+                logger.error('URLError = ' + str(e.reason))
+            except Exception:
+                import traceback
+                logger.error('generic exception: ' + traceback.format_exc())
+
+    def get(self, url):
+        try:
+            return urllib2.urlopen(url).read()
+        except urllib2.HTTPError as e:
+            logger.error('HTTPError = ' + str(e.code))
+        except urllib2.URLError as e:
+            logger.error('URLError = ' + str(e.reason))
+        except Exception:
+            import traceback
+            logger.error('generic exception: ' + traceback.format_exc())
