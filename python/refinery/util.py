d26c01a89f238062caed2894800ec0c7ec467295
Joseph Allemandou <joal@wikimedia.org>
2018-09-11 14:01:43 +0200

    Update python/refinery/utils/HdfsUtils
    Add basic some missing functions (get, touchz)
    Update some existing ones (ls now provide a detailed version)
    Add an rsync function allowing to rsync glob patterns between
    HDFS and local folders (both ways).
    
    Bug: T202489
    Change-Id: I7d2de9967eba2e3a809bdcf1512b51a2a844dcc2

--- python/refinery/util.py
+++ python/refinery/util.py
@@ -26,6 +26,7 @@ import subprocess
 import re
 import tempfile
 import json
+import glob
 
 # Ugly but need python3 support
 try:
@@ -583,10 +584,8 @@ class HivePartition(OrderedDict):
 
 
 class HdfsUtils(object):
-    # TODO:  Use snakebite instead of shelling out to 'hdfs dfs'.
-
     @staticmethod
-    def ls(paths, include_children=True):
+    def ls(paths, include_children=True, with_details=False):
         """
         Runs hdfs dfs -ls on paths.
 
@@ -605,8 +604,8 @@ class HdfsUtils(object):
         if not include_children:
             options.append('-d')
 
-        return [
-            line.split()[-1] for line in sh(
+        split_lines = [
+            line.split() for line in sh(
                 ['hdfs', 'dfs', '-ls'] + options + paths,
                 # Not checking return code here so we don't
                 # fail paths do not exist.
@@ -614,15 +613,33 @@ class HdfsUtils(object):
             ).splitlines() if not line.startswith(b'Found ')
         ]
 
+        if with_details:
+            return [
+                {
+                    'file_type': 'f' if parts[0].decode('utf-8')[0] == '-' else 'd',
+                    'permission': parts[0][1:],
+                    'replication': parts[1],
+                    'owner': parts[2],
+                    'group': parts[3],
+                    'file_size': parts[4],
+                    'modification_date': parts[5],
+                    'modification_time': parts[6],
+                    'path': parts[7]
+                } for parts in split_lines
+            ]
+        else:
+            return [parts[-1] for parts in split_lines]
+
     @staticmethod
-    def rm(paths):
+    def rm(paths, recurse=True, skip_trash=True):
         """
-        Runs hdfs dfs -rm -R -skipTrash on paths.
+        Runs hdfs dfs -rm -R on paths, optinally skipping trash.
         """
         if isinstance(paths, str):
             paths = paths.split()
 
-        return sh(['hdfs', 'dfs', '-rm', '-R', '-skipTrash'] + paths)
+        options = (['-R'] if recurse else []) + (['-skipTrash'] if skip_trash else [])
+        return sh(['hdfs', 'dfs', '-rm'] + options + paths)
 
     @staticmethod
     def rmdir(paths):
@@ -634,15 +651,15 @@ class HdfsUtils(object):
 
         return sh(['hdfs', 'dfs', '-rmdir'] + paths)
 
-    @staticmethod
-    def mkdir(paths):
+    def mkdir(paths, create_parent=True):
         """
-        Runs hdfs dfs -rm -R on paths.
+        Runs hdfs dfs -mkdir -p on paths.
         """
+        options = ['-p'] if create_parent else []
         if isinstance(paths, str):
             paths = paths.split()
 
-        return sh(['hdfs', 'dfs', '-mkdir', '-p'] + paths)
+        return sh(['hdfs', 'dfs', '-mkdir'] + options + paths)
 
     @staticmethod
     def cp(fromPath, toPath, force=False):
@@ -655,41 +672,46 @@ class HdfsUtils(object):
         sh(command)
 
     @staticmethod
-    def mv(fromPaths, toPaths, inParent=True):
+    def mv(from_paths, to_paths, inParent=True):
         """
         Runs hdfs dfs -mv fromPath toPath for each values of from/to Paths.
         If inParent is True (default), the parent folder in each of the
-        toPaths provide is used as destination. Set inParent parameter to
+        to_paths provide is used as destination. Set inParent parameter to
         False if the file/folder moved is also renamed.
         """
-        if isinstance(fromPaths, str):
-            fromPaths = fromPaths.split()
+        if isinstance(from_paths, str):
+            from_paths = from_paths.split()
 
-        if isinstance(toPaths, str):
-            toPaths = toPaths.split()
+        if isinstance(to_paths, str):
+            to_paths = to_paths.split()
 
-        if len(fromPaths) != len(toPaths):
-            raise Exception('fromPaths and toPaths size don\'t match in hdfs mv function')
+        if len(from_paths) != len(to_paths):
+            raise Exception('from_paths and to_paths size don\'t match in hdfs mv function')
 
-
-        for i in range(len(fromPaths)) :
-            toParent = '/'.join(toPaths[i].split('/')[:-1])
+        for i in range(len(from_paths)) :
+            toParent = '/'.join(to_paths[i].split('/')[:-1])
             if not HdfsUtils.ls(toParent, include_children=False):
                 HdfsUtils.mkdir(toParent)
             if (inParent):
-                sh(['hdfs', 'dfs', '-mv', fromPaths[i], toParent])
+                sh(['hdfs', 'dfs', '-mv', from_paths[i], toParent])
             else:
-                sh(['hdfs', 'dfs', '-mv', fromPaths[i], toPaths[i]])
+                sh(['hdfs', 'dfs', '-mv', from_paths[i], to_paths[i]])
 
     @staticmethod
-    def put(localPath, hdfsPath, force=False):
+    def put(local_path, hdfs_path, force=False):
         """
-        Runs 'hdfs dfs -put localPath hdfsPath' to copy a local file over to hdfs.
+        Runs 'hdfs dfs -put local_path hdfs_path' to copy a local file over to hdfs.
         """
-        command = ['hdfs', 'dfs', '-put', localPath, hdfsPath]
-        if force:
-            command.insert(3, '-f')
-        sh(command)
+        options = ['-f'] if force else []
+        sh(['hdfs', 'dfs', '-put'] + options + [local_path, hdfs_path])
+
+    @staticmethod
+    def get(hdfs_path, local_path, force=False):
+        """
+        Runs 'hdfs dfs -get hdfs_path local_path' to copy a local file over to hdfs.
+        """
+        options = ['-f'] if force else []
+        sh(['hdfs', 'dfs', '-get'] + options + [hdfs_path, local_path])
 
     @staticmethod
     def cat(path):
@@ -709,6 +731,16 @@ class HdfsUtils(object):
         date_str, time_str = stat_str.strip().split()
         iso_datetime_str = date_str + 'T' + time_str + 'Z'
         return parser.parse(iso_datetime_str)
+
+    @staticmethod
+    def touchz(paths):
+        """
+        Runs hdfs dfs -touchz paths, optinally skipping trash.
+        """
+        if isinstance(paths, str):
+            paths = paths.split()
+
+        return sh(['hdfs', 'dfs', '-touchz'] + paths)
 
     @staticmethod
     def validate_path(path):
@@ -734,6 +766,180 @@ class HdfsUtils(object):
         Returns the size in bytes of a hdfs path
         """
         return int(sh(['hdfs', 'dfs', '-du', '-s', path]).split()[0])
+
+    @staticmethod
+    def rsync(local_path, hdfs_path, local_to_hdfs=True,
+              should_delete=False, dry_run=True):
+        """
+        Copy files from source to destination (either local-to-hdfs or vice-versa
+        depending on local_to_hdfs parameter) trying to minimise copies using
+        file-size as a reference.
+        Destination is checked to be an existing folder and source is expected
+        to be a path or glob.
+        Destination files present in source but whose size or type don't match
+        source will be deleted and reimported.
+        Both source and destination should be absolute.
+        If should_delete is set to True, files in destination not present in source
+        will be deleted.
+        WARNING: If a glob pattern is used at folder-level, files will be copied
+                 but not the folder hierarchy. This also means that if files have
+                 the same name in different globed-folders, only one will remain
+                 in the destination folder.
+
+        """
+        # Check destination being a folder
+        if ((local_to_hdfs and not HdfsUtils._check_hdfs_dir(hdfs_path)) or
+                (not local_to_hdfs and not HdfsUtils._check_local_dir(local_path))):
+            return False
+
+        # Get files to copy by comparing src and dst files lists
+        files_left_to_copy = HdfsUtils._files_left_to_copy(
+            local_path, hdfs_path, local_to_hdfs, should_delete, dry_run)
+        if len(files_left_to_copy) == 0:
+            logger.info('No file to copy'.format(len(files_left_to_copy)))
+            return True
+
+        # Define copy and dst_path depending on local_to_hdfs
+        if local_to_hdfs:
+            (copy, dst_path) = (HdfsUtils.put, hdfs_path)
+        else:
+            (copy, dst_path) = (HdfsUtils.get, local_path)
+
+        # Do the actual copy
+        logger.info('Copying {} files ...'.format(len(files_left_to_copy)))
+        for f in files_left_to_copy:
+            src_file = files_left_to_copy[f]['path']
+            logger.info('Copying {} to {}'.format(src_file, dst_path))
+            if not dry_run:
+                copy(src_file, dst_path)
+
+        # Check copy to return value
+        files_left_to_copy = HdfsUtils._files_left_to_copy(
+            local_path, hdfs_path, local_to_hdfs, should_delete, dry_run)
+        if len(files_left_to_copy) == 0:
+            logger.info('Successfull copy')
+            return True
+        else:
+            logger.error('Copy not successfull, files are still to be copied: ' +
+                         ','.join(files_left_to_copy))
+            return False
+
+    @staticmethod
+    def _check_local_dir(local_path):
+        """
+        Returns True if given parameter is an existing local directory
+        """
+        if not os.path.isdir(local_path):
+            logger.error('Local destination folder ' + local_path +
+                         ' either doesn\'t exists or is not a directory')
+            return False
+        return True
+
+    @staticmethod
+    def _check_hdfs_dir(hdfs_path):
+        """
+        Returns True if given parameter is an existing hdfs directory
+        """
+        output_details = HdfsUtils.ls(hdfs_path, include_children=False, with_details=True)
+        if not output_details or output_details[0]['file_type'] != 'd':
+            logger.error('HDFS destination folder ' + hdfs_path +
+                         ' either doesn\'t exists or is not a directory')
+            return False
+        return True
+
+    @staticmethod
+    def _files_left_to_copy(local_path, hdfs_path, local_to_hdfs,
+                            should_delete, dry_run):
+        """
+        Builds the list of files to copy from source to destination.
+        List files from source and destination and return only files
+        that are in source and not in destination.
+        A check on file-type and file-size is made for files being in source
+        and destination. In case of a difference, destination file is overwritten.
+        In case a file exists in destination folder but not in source, it is
+        delete id should_delete is True, kept otherwise.
+        """
+        logger.info('Building lists of files to rsync from {} to {}'.format(
+            local_path if local_to_hdfs else hdfs_path,
+            hdfs_path if local_to_hdfs else local_path))
+
+        # Get hdfs files
+        hdfs_files = HdfsUtils._get_hdfs_files(hdfs_path)
+
+        # Get local files using local_path as a glob except if it is a folder
+        local_glob = os.path.join(local_path, '*') if os.path.isdir(local_path) else local_path
+        local_files = HdfsUtils._get_local_files(local_glob)
+
+        if local_to_hdfs:
+            (src_files, dst_files) = (local_files, hdfs_files)
+            rm = HdfsUtils.rm
+        else:
+            (src_files, dst_files) = (hdfs_files, local_files)
+            rm = os.remove
+
+        for existing_file in dst_files:
+            dst_file = dst_files[existing_file]
+            # file exists in src_files
+            if existing_file in src_files:
+                src_file = src_files[existing_file]
+                # Same file-type and file-size (except for dirs)
+                if (dst_file['file_type'] == src_file['file_type'] and
+                        (dst_file['file_type'] == 'd' or
+                            dst_file['file_size'] == src_file['file_size'])):
+                    src_files.pop(existing_file, None)
+                # Corrupted file - delete if should_delete or raise error
+                else:
+                    logger.info('Deleting {} '.format(dst_files[existing_file]['path']) +
+                                'for being different from its source conterpart ' +
+                                '(incorrect file type or size)')
+                    if not dry_run:
+                        rm(dst_files[existing_file]['path'])
+            # file not present in to_be_imported list, delete if should_delete
+            elif should_delete:
+                logger.info('Deleting {} '.format(dst_files[existing_file]['path']) +
+                            'for not being in the import list')
+                if not dry_run:
+                    rm(dst_files[existing_file]['path'])
+            else:
+                logger.info('File {} '.format(dst_files[existing_file]['path']) +
+                            'is not in the import list')
+
+        return src_files
+
+
+    @staticmethod
+    def _get_hdfs_files(hdfs_path):
+        """
+        List HDFS-files in path/glob parameter.
+        Return a dictionnary keyed by filename and having dictionnary values
+        of {'path', 'file_type', 'file_size'} (same as _get_local_files)
+        """
+        hdfs_files = {}
+        # HDFS-ls works with path and globs
+        for f in HdfsUtils.ls(hdfs_path, with_details=True):
+            # HDFS-ls returns bytes - Need to extract
+            path = f['path'].decode("utf-8")
+            file_size = int(f['file_size'].decode("utf-8"))
+            file_type = f['file_type']
+            fname = os.path.basename(path)
+            hdfs_files[fname] = {'path': path, 'file_size': file_size, 'file_type': file_type}
+        return hdfs_files
+
+    @staticmethod
+    def _get_local_files(local_glob):
+        """
+        List local-files in path/glob parameter.
+        Return a dictionnary keyed by filename and having dictionnary values
+        of {'path', 'file_type', 'file_size'} (same as _get_hdfs_files)
+        """
+        local_files = {}
+        for f in glob.glob(local_glob):
+            fname = os.path.basename(f)
+            file_size = os.path.getsize(f)
+            file_type = 'd' if os.path.isdir(f) else 'f'
+            local_files[fname] = {'path': f, 'file_size': file_size, 'file_type': file_type}
+        return local_files
+
 
 class DruidUtils(object):
     def __init__(self, druid_host):
@@ -765,7 +971,7 @@ class DruidUtils(object):
         """
         If deep is false, just sends a DELETE request to the druid coordinator
         for the selected datasource, therefore just disabling it but not deleting
-        it from deep storage. Returns the response body of the request if 
+        it from deep storage. Returns the response body of the request if
         it was successful.
 
         If deep is true, removes each interval in the datasource, effectively deleting
