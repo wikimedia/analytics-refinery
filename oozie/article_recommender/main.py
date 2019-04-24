60bca5198418deb238b87358951dfb6ea161777a
Baha <bmansurov@wikimedia.org>
2019-04-24 15:07:21 -0400

    Oozie article recommender: use version 0.0.2
    Page views are generated at once which makes the job run much faster.
    
    Bug: T210844
    Change-Id: I2eaf1092dbdba5969cdcbbcb3cc09c0c5ad5bd40

--- oozie/article_recommender/main.py
+++ oozie/article_recommender/main.py
@@ -1,10 +1,7 @@
 import argparse
 from datetime import datetime
-import logging
 
-from article_recommender import recommend
-
-from pyspark.sql import SparkSession
+from article_recommender import normalizedscores as ns, util
 
 
 def get_cmd_options():
@@ -16,18 +13,24 @@ def get_cmd_options():
     """
     parser = argparse.ArgumentParser(
         description='Generates article normalized scores.')
+    parser.add_argument('language_pairs',
+                        help='Comma separated list of source and target '
+                        'language codes, e.g. ru-uz,en-ko,ko-uz.',
+                        type=lambda pairs: [
+                            x.split('-') for x in pairs.split(',')
+                        ])
     parser.add_argument('end_date',
                         help='End date in the yyyymmdd format.',
                         type=lambda x: datetime.strptime(x, "%Y%m%d").date())
     parser.add_argument('--spark_app_name',
                         help='Name of the spark application, e.g.' +
                         ' "article-recommender"')
-    parser.add_argument('--language_pairs_file',
-                        help='Location of the list of language pairs to train.')
-    parser.add_argument('--wikidata_dir',
-                        help='Location of Wikidata dumps in HDFS.')
     parser.add_argument('--topsites_file',
                         help='Location of top Wikipedias by edit count.')
+    parser.add_argument('--dblist_file',
+                        help='Location of list of Wikipedias.')
+    parser.add_argument('--wikidata_dir',
+                        help='Location of Wikidata dumps in HDFS.')
     parser.add_argument('--output_dir',
                         help='Output location in HDFS.')
     parser.add_argument('--tmp_dir',
@@ -43,41 +46,26 @@ def validate_cmd_options(options):
 
     """
     if options.end_date > datetime.today().date():
-        logging.error('End date cannot be later than today: %s.' %
-                      options.end_date)
+        print('End date cannot be later than today: %s.' %
+              options.end_date)
         return False
     return True
-
-
-def get_language_pairs_to_train(spark, file):
-    return spark.read.load(file,
-                           format="csv", sep="\t",
-                           inferSchema="true", header="true").collect()
 
 
 def main():
     options = get_cmd_options()
     if validate_cmd_options(options):
-        spark = SparkSession\
-            .builder\
-            .appName(options.spark_app_name)\
-            .enableHiveSupport()\
-            .getOrCreate()
-        for source_language, target_language in\
-                get_language_pairs_to_train(spark, options.language_pairs_file):
-            print('Started trainig %s-%s with ending date %s.' %
-                  (source_language, target_language, options.end_date))
-            normalized_scores = recommend.NormalizedScores(
-                spark,
-                source_language,
-                target_language,
-                options.end_date,
-                options.wikidata_dir,
-                options.topsites_file,
-                options.output_dir,
-                options.tmp_dir
-            )
-            normalized_scores.train()
+        spark = util.get_spark_session(options.spark_app_name)
+        normalized_scores = ns.NormalizedScores(
+            spark,
+            options.language_pairs,
+            options.end_date,
+            options.wikidata_dir,
+            options.topsites_file,
+            options.output_dir,
+            options.tmp_dir
+        )
+        normalized_scores.train()
 
 
 if __name__ == '__main__':
